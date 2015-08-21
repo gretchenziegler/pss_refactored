03ba670b02feeca6dc00a0eea57301a1a632b9dc
gretchenziegler <gretchenziegler@gmail.com>
2015-08-20 20:56:31 -0400

    Enable authentication

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,7 +1,19 @@
 class MembersController < ApplicationController
 
 	def index
+		@member = Member.find(session[:member_id])
+	end
 
+	def bylaws
+	end
+
+	def edit
+	end
+
+	def schedule
+	end
+
+	def tracks
 	end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ Rails.application.routes.draw do
 
   resources :session, only: [:create, :destroy, :new]
 
-  resources :members, only: [:edit, :show]
+  resources :members
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
